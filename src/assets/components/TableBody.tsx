e392ac758e8eaea824d3fe4463837194d52a58f0
Martha  avila <itit.martha.avil@gmail.com>
2024-07-01 01:30:50 -0600

    styles and improves UI

--- src/assets/components/TableBody.tsx
+++ src/assets/components/TableBody.tsx
@@ -8,14 +8,14 @@ const TableBody: React.FC<TableBodyProps> = (props) => {
    
     return(<div className="overflow-x-auto">
       <table className="min-w-full bg-white border border-gray-200 rounded-lg">
-        <thead className="bg-gray-100">
+        <thead className="bg-sky-600 border border-sky-600">
           <tr>
-            <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-600">Id</th>
-            <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-600">Nombre</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-600">Email</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-600">Genero</th>
-                                                <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-600">Estatus</th>
-                                    <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-600">Acciones</th>
+            <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-50">Id</th>
+            <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-50">Nombre</th>
+                        <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-50">Email</th>
+                        <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-50">Genero</th>
+                                                <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-50">Estatus</th>
+                                    <th className="py-2 px-4 border-b border-gray-200 text-left text-gray-50">Acciones</th>
           </tr>
         </thead>
              <tbody>
